32b4ae7f323d96d77bf9c90df2cfc0a557d01547
torishima <torikou3685@gmail.com>
2022-08-12 23:41:29 +0900

    View ActionでAPI叩いてappendまで

--- Architecture_MVVM/Scurce/Presentation/Screen/Home/Search/SearchViewController.swift
+++ Architecture_MVVM/Scurce/Presentation/Screen/Home/Search/SearchViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import Combine
 
 final class SearchViewController: UIViewController {
     
     var viewModel: SearchViewModel!
+    private var cancelables = Set<AnyCancellable>()
     private var searchController: UISearchController!
     
     override func viewDidLoad() {
@@ -24,13 +26,46 @@ final class SearchViewController: UIViewController {
     }
     
     private func bind() {
+        viewModel.$displayState
+            .sink { state in
+                switch state {
+                case .initial:
+                    print("initial")
+                case .main:
+                    print("main")
+                }
+            }.store(in: &cancelables)
+        viewModel.$progressState
+            .sink { state in
+                switch state {
+                case .none:
+                    print("none")
+                case .fetch:
+                    print("fetch")
+                }
+            }.store(in: &cancelables)
+//        viewModel.isSearched
+//            .sink { _ in
+//
+//
+//            }.store(in: &cancelables)
+        viewModel.$datasource
+            .sink { datas in
+                // リロード
+                print(datas.count)
+            }.store(in: &cancelables)
         
     }
     
     private func setupSearchController() {
+        // 検索結果を別の画面に表示することができる
+        // 検索結果表示用画面が特になければ、ここは入れなくて良い
+        // 検索結果と初期表示の内容を分けたければ別で良さそう
         searchController = UISearchController(searchResultsController: nil)
         searchController.delegate = self
         searchController.searchBar.delegate = self
+        // フォーカスの色と、キャンセルの文言の色が変わる
+        searchController.searchBar.tintColor = .lightGray
         navigationItem.searchController = searchController
     }
 }
@@ -40,34 +75,40 @@ extension SearchViewController: UISearchBarDelegate {
         guard let searchText = searchBar.text else {
             return
         }
-        
-        print(searchText)
-        // viewModelに渡す
-    }
-}
-
-extension SearchViewController: UISearchControllerDelegate {
-    func presentSearchController(_ searchController: UISearchController) {
-        print("searchbarをタップして一番最初に起動")
-    }
-    
-    func willPresentSearchController(_ searchController: UISearchController) {
-        print("presentSearchControllerメソッドの後に呼ばれる")
-    }
-    
-    func didPresentSearchController(_ searchController: UISearchController) {
-        print("フォーカス当た理終わってキーボード開いたら")
-    }
-    
-    func willDismissSearchController(_ searchController: UISearchController) {
-        print("キャンセルボタンを押してすぐ")
-    }
-    
-    func didDismissSearchController(_ searchController: UISearchController) {
-        print("キーボード閉じ終わってから")
+        viewModel.apply(searchText: searchText)
     }
 }
 
 extension SearchViewController: SearchRouter {
     
+}
+
+// ---------------------------------------------------------------- //
+/*
+ UISearchControllerを使うときは以下のような動きをする
+ 表示されている画面の階層の上に新しいコントローラー画面を出現させることができる
+ 検索してその画面で表示するだけの場合は、UISearchControllerで対応しなくても良い
+ 
+ */
+
+extension SearchViewController: UISearchControllerDelegate {
+    func presentSearchController(_ searchController: UISearchController) {
+        //print("searchbarをタップして一番最初に起動")
+    }
+    
+    func willPresentSearchController(_ searchController: UISearchController) {
+        //print("presentSearchControllerメソッドの後に呼ばれる")
+    }
+    
+    func didPresentSearchController(_ searchController: UISearchController) {
+        //print("フォーカス当た理終わってキーボード開いたら")
+    }
+    
+    func willDismissSearchController(_ searchController: UISearchController) {
+        //print("キャンセルボタンを押してすぐ")
+    }
+    
+    func didDismissSearchController(_ searchController: UISearchController) {
+        //print("キーボード閉じ終わってから")
+    }
 }
--- Architecture_MVVM/Scurce/Presentation/Screen/Home/Search/SearchViewModel.swift
+++ Architecture_MVVM/Scurce/Presentation/Screen/Home/Search/SearchViewModel.swift
@@ -6,11 +6,25 @@
 //
 
 import Foundation
+import Combine
 
 @MainActor
 final class SearchViewModel {
     
+    enum DisplayState {
+        case initial
+        case main
+    }
+    
+    enum ProgressState {
+        case none
+        case fetch
+    }
+    
     // Publisher
+    @Published private (set) var displayState: DisplayState = .initial
+    @Published private (set) var progressState: ProgressState = .none
+    @Published private (set) var datasource: [Repository] = []
     
     private let searchRepository: SearchRepository
     private weak var router: SearchRouter?
@@ -20,22 +34,39 @@ final class SearchViewModel {
     }
     
     func viewDidLoard() {
-        fetchRepositories()
+        
     }
     
     func set(router: SearchRouter) {
         self.router = router
     }
     
-    private func fetchRepositories() {
+    func apply(searchText: String) {
+        reset()
+        fetchRepositories(query: searchText)
+    }
+    
+    // 検索して最初のfetch
+    private func fetchRepositories(query: String) {
+        progressState = .fetch
         Task {
             do {
-                let response =  try await searchRepository.getSearchRepositories(query: "go")
-                print(response)
+                defer { Task { @MainActor in progressState = .none } }
+                let response =  try await searchRepository.getSearchRepositories(query: query)
+                datasource.append(contentsOf: response.items)
                 
             } catch let error {
-                print(error)
+                print(error.localizedDescription)
             }
         }
+    }
+    
+    // スクロールして取りに行く時によぶ
+    private func hasnext() {
+        
+    }
+    
+    private func reset() {
+        datasource.removeAll()
     }
 }
